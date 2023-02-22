7f14d418f5bab06683bc06a57e1002388315ae95
Bengisu Özaydın <bengisuozaydin@gmail.com>
2023-02-21 17:32:22 -0700

    add docker to fuelhat

--- packages/fuelhat/commands/start.mjs
+++ packages/fuelhat/commands/start.mjs
@@ -1,70 +1,72 @@
 /* eslint-disable no-console */
-import c from 'chalk';
-import { spawn } from 'child_process';
-import path from 'path';
-import Spinnies from 'spinnies';
-import { fileURLToPath } from 'url';
+import c from "chalk";
+import { spawn } from "child_process";
+import path from "path";
+import Spinnies from "spinnies";
+import { fileURLToPath } from "url";
 
-import { createEnv } from '../utils/createEnv.mjs';
+import { createEnv } from "../utils/createEnv.mjs";
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 const resolve = (str) => path.resolve(dirname, str);
-const spinnies = new Spinnies({ succeedPrefix: '⚡️ ' });
+const spinnies = new Spinnies({ succeedPrefix: "⚡️ " });
 
 function label(str) {
-  return c.green.bold(str);
+    return c.green.bold(str);
 }
 
 export const handler = async (argv) => {
-  const {
-    filepath: envPath,
-    port,
-    faucetPort,
-    projectName,
-  } = await createEnv(argv);
+    const {
+        filepath: envPath,
+        port,
+        faucetPort,
+        projectName,
+    } = await createEnv(argv);
 
-  const isTest = argv.t;
-  const isDebug = argv.d;
+    const isTest = argv.t;
+    const isDebug = argv.d;
 
-  const args = [
-    'run',
-    '--ip',
-    '127.0.0.1',
-    '--port',
-    '4000',
-    '--chain',
-    '../fuel-core/chainConfig.json',
-    '--db-type',
-    'in-memory',
-  ];
+    const file = ["-f", resolve("../docker/docker-compose.yml")];
+    const args = [
+        ...file,
+        "--env-file",
+        envPath,
+        "-p",
+        projectName,
+        "up",
+        "--build",
+        "-d",
+    ];
 
-  if (!isDebug) {
-    spinnies.add('1', { text: 'Starting Fuel local node...' });
-  }
-
-  const process = spawn(
-    'fuel-core',
-    args,
-    isDebug && { stdio: 'inherit' }
-  );
-
-  process.stdout?.on('data', (data) => {
-    console.log(c.cyan(data));
-  });
-  process.stdout?.on('end', () => {
     if (!isDebug) {
-      console.log('\n');
-      spinnies.succeed('1', { text: 'Fuel node running locally!' });
-      console.log(c.gray('----'));
+        spinnies.add("1", { text: "Starting Fuel local node..." });
     }
-    console.log(
-      `${label('⇢ Environment:')} ${isTest ? 'Test' : 'Development'}`
+
+    const process = spawn(
+        "docker-compose",
+        args,
+        isDebug && { stdio: "inherit" }
     );
-    console.log(`${label('⇢ Provider URL:')} http://localhost:${port}/graphql`);
-    console.log(
-      `${label('⇢ Faucet URL:')} http://localhost:${faucetPort}/dispense`
-    );
-  });
+
+    process.stdout?.on("data", (data) => {
+        console.log(c.cyan(data));
+    });
+    process.stdout?.on("end", () => {
+        if (!isDebug) {
+            console.log("\n");
+            spinnies.succeed("1", { text: "Fuel node running locally!" });
+            console.log(c.gray("----"));
+        }
+        console.log(
+            `${label("⇢ Environment:")} ${isTest ? "Test" : "Development"}`
+        );
+        console.log(
+            `${label("⇢ Provider URL:")} http://localhost:${port}/graphql`
+        );
+        console.log(
+            `${label("⇢ Faucet URL:")} http://localhost:${faucetPort}/dispense`
+        );
+    });
 };
 
-export const command = 'start';
+export const command = "start";
