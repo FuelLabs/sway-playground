eee2a1c819132819b58f637a8b42911c50feaac2
Nick <thenickdodson@gmail.com>
2024-03-27 03:10:48 -0700

    Update index.ts

--- app/src/features/editor/examples/sway/index.ts
+++ app/src/features/editor/examples/sway/index.ts
@@ -1,9 +1,10 @@
 import { ExampleMenuItem } from "../../components/ExampleDropdown";
 import { EXAMPLE_SWAY_CONTRACT_COUNTER } from "./counter";
-import { EXAMPLE_SWAY_CONTRACT_SRC20 } from "./src20";
+import { EXAMPLE_SWAY_CONTRACT_SINGLEASSET } from "./src20";
 
 export const EXAMPLE_SWAY_CONTRACTS: ExampleMenuItem[] = [
     { label: 'Counter.sw', code: EXAMPLE_SWAY_CONTRACT_COUNTER },
-    { label: 'SRC20.sw', code: EXAMPLE_SWAY_CONTRACT_SRC20 },
+    { label: 'SingleAsset.sw', code: EXAMPLE_SWAY_CONTRACT_SINGLEASSET },
+    { label: 'MultiAsset.sw', code: EXAMPLE_SWAY_CONTRACT_MULTIASSET },
   ];
   
