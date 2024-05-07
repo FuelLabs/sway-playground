f728474fc55adcc1b18fc3a343d850e051914136
Matt Auer <mattauer@umich.edu>
2024-05-07 13:41:06 -0500

    chore: add external domain url

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,6 +1,10 @@
 export const FUEL_GREEN = '#00f58c';
 
 export const LOCAL_SERVER_URI = 'http://0.0.0.0:8080';
-export const SERVER_URI = process.env.REACT_APP_LOCAL_SERVER
+export const IS_LOCAL = !!process.env.REACT_APP_LOCAL_SERVER;
+export const SERVER_URI = IS_LOCAL
   ? LOCAL_SERVER_URI
   : 'https://api.sway-playground.org';
+export const APPROVED_EXTERNAL_DOMAIN = IS_LOCAL
+  ? 'http://localhost:3000' // Local domain of docs hub
+  : 'https://docs-hub.vercel.app';
