0db08e2034b6f54e00878890cc00435e76011bc7
Sophie <47993817+sdankel@users.noreply.github.com>
2024-12-30 18:36:28 -0800

    prettier

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,8 +1,9 @@
 const SERVER_API =
-  process.env.REACT_APP_SERVER_API || "https://api.sway-playground.fuel.network";
+  process.env.REACT_APP_SERVER_API ||
+  'https://api.sway-playground.fuel.network';
 
-export const FUEL_GREEN = "#00f58c";
-export const LOCAL_SERVER_URI = "http://0.0.0.0:8080";
+export const FUEL_GREEN = '#00f58c';
+export const LOCAL_SERVER_URI = 'http://0.0.0.0:8080';
 export const SERVER_URI = process.env.REACT_APP_LOCAL_SERVER
   ? LOCAL_SERVER_URI
   : SERVER_API;
