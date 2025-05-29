87b3ffdb6ad032bd3c22253d09a4efd60cab1071
Nick <thenickdodson@gmail.com>
2025-05-29 04:48:24 -0700

    Update index.ts

--- app/src/features/editor/examples/sway/index.ts
+++ app/src/features/editor/examples/sway/index.ts
@@ -1,10 +1,12 @@
 import { ExampleMenuItem } from "../../components/ExampleDropdown";
 import { EXAMPLE_SWAY_CONTRACT_COUNTER } from "./counter";
+import { EXAMPLE_SWAY_CONTRACT_LIQUIDITY_POOL } from "./liquiditypool";
 import { EXAMPLE_SWAY_CONTRACT_SINGLEASSET } from "./singleasset";
 import { EXAMPLE_SWAY_CONTRACT_MULTIASSET } from "./multiasset";
 
 export const EXAMPLE_SWAY_CONTRACTS: ExampleMenuItem[] = [
   { label: "Counter.sw", code: EXAMPLE_SWAY_CONTRACT_COUNTER },
+  { label: "LiquidityPool.sw", code: EXAMPLE_SWAY_CONTRACT_LIQUIDITY_POOL },
   { label: "SingleAsset.sw", code: EXAMPLE_SWAY_CONTRACT_SINGLEASSET },
   { label: "MultiAsset.sw", code: EXAMPLE_SWAY_CONTRACT_MULTIASSET },
 ];
